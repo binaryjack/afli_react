429a5365b1446f9262412921d0266559ad7d7c27
Tadeo Piana <pianatadeo@hotmail.com>
2021-10-14 10:09:57 +0200

    prettier config

--- src/lib/Compositions/Version/Version.tsx
+++ src/lib/Compositions/Version/Version.tsx
@@ -4,30 +4,28 @@ import { useDispatch, useSelector } from 'react-redux';
 import { CommonActionTypes } from 'Store/Common/CommonActions';
 import { ICommonState } from 'Store/Common/CommonReducer';
 const Version: FC = () => {
-    const version = useSelector<ICommonState>(state => state.version);
-    const dispatch = useDispatch();
-    const incrFunc = () => {
-        dispatch(
+  const version = useSelector<ICommonState>((state) => state.version);
+  const dispatch = useDispatch();
+  const incrFunc = () => {
+    dispatch({
+      type: CommonActionTypes.Increase,
+      payload: 1,
+    });
+  };
+  const decrFunc = () => {
+    dispatch({
+      type: CommonActionTypes.Increase,
+      payload: 1,
+    });
+  };
 
-            {
-                type: CommonActionTypes.Increase,
-                payload: 1
-            }
-        );
-    }
-    const decrFunc = () => {
-        dispatch(
-            {
-                type: CommonActionTypes.Increase,
-                payload: 1
-            }
-        );
-    }
-    return <>
-        <PTextBox text={`Version: ${version}`} />
-        <button onClick={incrFunc}>+</button>
-        <button onClick={decrFunc}>-</button>
+  return (
+    <>
+      <PTextBox text={`Version: ${version}`} />
+      <button onClick={incrFunc}>+</button>
+      <button onClick={decrFunc}>-</button>
     </>
+  );
 };
 
 export default Version;
